3252852491d9625f0bc75c25afde502e66c77514
Irene Musau <irenemwikalii04@gmail.com>
2025-08-20 22:21:45 +0300

    db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URI
+from models import Base
+
+engine = create_engine(DATABASE_URI, echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
